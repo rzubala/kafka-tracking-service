c5adbcacc5221ae5b62d6a5e2e3cda2e087905ed
Rafał Zubala <rzubala@gmail.com>
2023-11-05 22:10:46 +0100

    kafka configuration

--- src/main/java/dev/lydtech/tracking/handler/DispatchTrackingHandler.java
+++ src/main/java/dev/lydtech/tracking/handler/DispatchTrackingHandler.java
@@ -5,7 +5,9 @@ import dev.lydtech.tracking.service.TrackingService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
 
+@Component
 @RequiredArgsConstructor
 @Slf4j
 public class DispatchTrackingHandler {
