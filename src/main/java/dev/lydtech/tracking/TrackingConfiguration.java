c9f99f80f2ffafdb6df9032c436abc489e5112df
Rafał Zubala <rzubala@gmail.com>
2023-11-07 22:02:06 +0100

    multiple event types on the same topic

--- src/main/java/dev/lydtech/tracking/TrackingConfiguration.java
+++ src/main/java/dev/lydtech/tracking/TrackingConfiguration.java
@@ -22,6 +22,8 @@ import java.util.Map;
 @Configuration
 public class TrackingConfiguration {
 
+    private static String TRUSTED_PACKAGES = "dev.lydtech.dispatch.message";
+
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(ConsumerFactory<String, Object> consumerFactory) {
         ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
@@ -35,8 +37,8 @@ public class TrackingConfiguration {
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
         config.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
-        config.put(JsonDeserializer.VALUE_DEFAULT_TYPE, DispatchPreparing.class.getCanonicalName());
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(JsonDeserializer.TRUSTED_PACKAGES, TRUSTED_PACKAGES);
         return new DefaultKafkaConsumerFactory<>(config);
     }
 
--- src/main/java/dev/lydtech/tracking/handler/DispatchTrackingHandler.java
+++ src/main/java/dev/lydtech/tracking/handler/DispatchTrackingHandler.java
@@ -4,21 +4,23 @@ import dev.lydtech.dispatch.message.DispatchPreparing;
 import dev.lydtech.tracking.service.TrackingService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
+@KafkaListener(
+        id = "trackingConsumerClient",
+        topics = "dispatch.tracking",
+        groupId = "tracking.dispatch.tracking",
+        containerFactory = "kafkaListenerContainerFactory"
+)
 public class DispatchTrackingHandler {
     private final TrackingService trackingService;
 
-    @KafkaListener(
-            id = "trackingConsumerClient",
-            topics = "dispatch.tracking",
-            groupId = "tracking.dispatch.tracking",
-            containerFactory = "kafkaListenerContainerFactory"
-    )
+    @KafkaHandler
     public void listen(DispatchPreparing dispatchPreparing) {
         log.info("Received dispatch preparing event " + dispatchPreparing.toString());
         try {
